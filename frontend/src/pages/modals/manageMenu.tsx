99e5c07b52b71e73f2bdb5d562253550e4913723
Nikhil Kumar <nk90600@gmail.com>
2024-06-08 09:34:25 +0530

    FE - Added bulk creatio UI

--- frontend/src/pages/modals/manageMenu.tsx
+++ frontend/src/pages/modals/manageMenu.tsx
@@ -8,6 +8,7 @@
   SelectGroup,
   SelectItem,
   SelectLabel,
+  SelectSeparator,
   SelectTrigger,
   SelectValue,
 } from "../../components/ui/select";
@@ -44,8 +45,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/reducer";
 import { AppDispatch } from "../../redux/store";
 import { createMenu, updateMenu } from "../../redux/actions";
-import { useContext, useState } from "react";
+import { useContext, useDebugValue, useEffect, useState } from "react";
 import DrawerContext from "../../context/drawerContext";
+import { Separator } from "../../components/ui/separator";
+import { DownloadIcon } from "lucide-react";
+import generateSampleExcel from "../common/excelfile";
 const FormSchema = z.object({
   type: z.enum(["all", "mentions", "none"], {
     required_error: "You need to select a notification type.",
@@ -80,6 +84,8 @@ export function ManageMenu({ menu }: any) {
   });
 
   const [isLoading, setIsLoading] = useState(false);
+  const [currentStep, setCurrentStep] = useState("bulk");
+  const [newCategory, setNewCategory] = useState("bulk");
 
   const { setOpen }: any = useContext(DrawerContext);
   const dispatch: AppDispatch = useDispatch();
@@ -115,108 +121,140 @@ export function ManageMenu({ menu }: any) {
     }
   };
 
+  const handleCurrentStepClick = (step: any) => {
+    setCurrentStep(step);
+  };
+
+  const handleAddCatChange = (e: any) => {
+    console.log(e.target.value);
+    setNewCategory(e.target.value);
+  };
+  const handleAddClick = (e: any) => {
+    allMenuCategories.push({ title: newCategory });
+  };
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  type="text"
-                  className="w-full rounded border px-2 py-1"
-                  placeholder="Name of the dish"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <>
+      {currentStep == "bulk" && (
+        <BulkCreationMenu currentStepClick={handleCurrentStepClick} />
+      )}
 
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Textarea
-                  {...field}
-                  className="w-full rounded border px-2 py-1"
-                  placeholder="Description of the dish"
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      {currentStep == "one" && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      className="w-full rounded border px-2 py-1"
+                      placeholder="Name of the dish"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <div className="grid grid-cols-2 gap-4">
-          <FormField
-            control={form.control}
-            name="price"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Price</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    type="number"
-                    className="w-full rounded border px-2 py-1"
-                    placeholder="Price in local currency"
-                    onChange={(e) => field.onChange(Number(e.target.value))}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      className="w-full rounded border px-2 py-1"
+                      placeholder="Description of the dish"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="categoryId"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Category</FormLabel>
-                <FormControl>
-                  <Select
-                    //   onValueChange={handleDesginationChange}
-                    defaultValue={
-                      !menu?.id && allMenuCategories?.length
-                        ? allMenuCategories[0]?.title
-                        : allMenuCategories?.find(
-                            (val) => val.id == menu?.categoryId
-                          ).title
-                    }
-                    onValueChange={field.onChange}
-                  >
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
                     <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a verified email to display" />
-                      </SelectTrigger>
+                      <Input
+                        {...field}
+                        type="number"
+                        className="w-full rounded border px-2 py-1"
+                        placeholder="Price in local currency"
+                        onChange={(e) => field.onChange(Number(e.target.value))}
+                      />
                     </FormControl>
-                    <SelectContent>
-                      {allMenuCategories.map((item) => {
-                        return (
-                          <SelectItem value={item.title}>
-                            {item.title}
-                          </SelectItem>
-                        );
-                      })}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-        {/* <FormField
+              <FormField
+                control={form.control}
+                name="categoryId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <FormControl>
+                      <Select
+                        //   onValueChange={handleDesginationChange}
+                        defaultValue={
+                          !menu?.id && allMenuCategories?.length
+                            ? allMenuCategories[0]?.title
+                            : allMenuCategories?.find(
+                                (val) => val.id == menu?.categoryId
+                              ).title
+                        }
+                        onValueChange={field.onChange}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a verified email to display" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {/* <div className="p-2 ">
+                            <div className="space-y-2">
+                              <Label htmlFor="new-item">Add a new item</Label>
+                              <div className="flex gap-2">
+                                <Input
+                                  id="new-item"
+                                  placeholder="Enter a new category"
+                                  onChange={handleAddCatChange}
+                                />
+                                <Button onClick={handleAddClick}>Add</Button>
+                              </div>
+                            </div>
+                          </div>
+
+                          <SelectSeparator></SelectSeparator> */}
+                          {allMenuCategories.map((item) => {
+                            return (
+                              <SelectItem value={item.title}>
+                                {item.title}
+                              </SelectItem>
+                            );
+                          })}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* <FormField
           control={form.control}
           name="available"
           render={({ field }) => (
@@ -234,73 +272,145 @@ export function ManageMenu({ menu }: any) {
           )}
         /> */}
 
-        <div className="grid grid-cols-2 gap-4">
-          <FormField
-            control={form.control}
-            name="spiceLevel"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Spice Level</FormLabel>
-                <FormControl>
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                    className="flex flex-col space-y-1"
-                  >
-                    {["Mild", "Medium", "Spicy"].map((spice) => (
-                      <div className="flex items-center space-x-2">
-                        <RadioGroupItem key={spice} value={spice} id={spice} />
-                        <Label
-                          className="font-normal cursor-pointer"
-                          htmlFor={spice}
-                        >
-                          {" "}
-                          {spice}{" "}
-                        </Label>
-                      </div>
-                    ))}
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="dietType"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Diet Type</FormLabel>
-                <FormControl>
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                    className="flex flex-col space-y-1"
-                  >
-                    {["Vegetarian", "Non-Vegetarian", "Vegan"].map((spice) => (
-                      <div className="flex items-center space-x-2">
-                        <RadioGroupItem key={spice} value={spice} id={spice} />
-                        <Label
-                          className="font-normal cursor-pointer"
-                          htmlFor={spice}
-                        >
-                          {" "}
-                          {spice}{" "}
-                        </Label>
-                      </div>
-                    ))}
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="spiceLevel"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Spice Level</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col space-y-1"
+                      >
+                        {["Mild", "Medium", "Spicy"].map((spice) => (
+                          <div className="flex items-center space-x-2">
+                            <RadioGroupItem
+                              key={spice}
+                              value={spice}
+                              id={spice}
+                            />
+                            <Label
+                              className="font-normal cursor-pointer"
+                              htmlFor={spice}
+                            >
+                              {" "}
+                              {spice}{" "}
+                            </Label>
+                          </div>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="dietType"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Diet Type</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col space-y-1"
+                      >
+                        {["Vegetarian", "Non-Vegetarian", "Vegan"].map(
+                          (spice) => (
+                            <div className="flex items-center space-x-2">
+                              <RadioGroupItem
+                                key={spice}
+                                value={spice}
+                                id={spice}
+                              />
+                              <Label
+                                className="font-normal cursor-pointer"
+                                htmlFor={spice}
+                              >
+                                {" "}
+                                {spice}{" "}
+                              </Label>
+                            </div>
+                          )
+                        )}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-        <Button loading={isLoading} type="submit">
-          Submit
-        </Button>
-      </form>
-    </Form>
+            <div className="flex gap-4">
+              <Button
+                variant={"secondary"}
+                onClick={() => setCurrentStep("bulk")}
+              >
+                Back
+              </Button>
+              <Button loading={isLoading} type="submit" className="w-full">
+                Submit
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </>
   );
 }
+
+export const BulkCreationMenu = ({ currentStepClick }: any) => {
+  const handleExcelDownload = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    generateSampleExcel();
+  };
+  return (
+    <>
+      <div className="grid gap-4">
+        <div className="grid gap-1">
+          {/* <FormLabel>Bulk Upload</FormLabel> */}
+          <Label htmlFor="file-upload">Bulk Upload</Label>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            Upload an Excel file to create multiple items at once.
+          </p>
+        </div>
+        <div className="flex items-center gap-4">
+          <Input id="file-upload" type="file" />
+          <Button>Upload</Button>
+        </div>
+
+        <div
+          onClick={(e) => handleExcelDownload(e)}
+          className="flex gap-2 bg-blue-100 items-center justify-between cursor-pointer  rounded-lg border p-3 shadow-sm"
+        >
+          {/* <FormLabel>Bulk Upload</FormLabel> */}
+          <div className="grid gap-1 ">
+            <p className="text-sm text-gray-500 dark:text-gray-400">
+              Download an Sample Excel file
+            </p>
+          </div>
+
+          <DownloadIcon className="w-4 h-4"></DownloadIcon>
+        </div>
+      </div>
+
+      <Separator orientation="horizontal" />
+      <div className="grid gap-4">
+        <div className="grid gap-1">
+          <Label htmlFor="create-item">Create Single Item</Label>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            Click the button to create a new item.
+          </p>
+        </div>
+        <Button id="create-item" onClick={() => currentStepClick("one")}>
+          Add Single Menu Item
+        </Button>
+      </div>
+    </>
+  );
+};
